66c9b629722f1250f7617e7424e19141a5b4f8b9
Mamoru <aberkunsky@gmail.com>
2020-11-23 01:35:16 +0200

    initial commit

---
+++ src/main/java/com/mamoru/quizbot/status/StatusController.java
@@ -0,0 +1,13 @@
+package com.mamoru.quizbot.status;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public Status status(){
+        return new Status("ok");
+    }
+}
---
+++ src/test/java/com/mamoru/quizbot/QuizbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamoru.quizbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
