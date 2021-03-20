1d02c2f5182262163543015b6033141db016a9fc
htearih <aberkunsky@gmail.com>
2021-03-21 01:38:51 +0200

    getOneSlava and remove master.yml

--- src/main/java/com/mamoru/hwslavabot/slava/SlavaController.java
+++ src/main/java/com/mamoru/hwslavabot/slava/SlavaController.java
@@ -2,6 +2,8 @@ package com.mamoru.hwslavabot.slava;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +17,11 @@ public class SlavaController {
     @GetMapping("/slava")
     public List<Slava> getSlavas(){
         return slavaRepository.findAll();
+    }
+
+    @GetMapping("/slava/{id}")
+    public Slava getSlava(@PathVariable("id") String id){
+        return slavaRepository.getOne(id);
     }
 
 
