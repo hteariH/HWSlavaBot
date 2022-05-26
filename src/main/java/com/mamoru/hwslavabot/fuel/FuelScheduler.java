9c52a28ec061112944fe84c870928cda5b90e38d
oberkunskyi <oleksii.berkunskyi@ocado.com>
2022-05-26 18:40:58 +0100

    qq

---
+++ src/main/java/com/mamoru/hwslavabot/fuel/FuelScheduler.java
@@ -0,0 +1,20 @@
+package com.mamoru.hwslavabot.fuel;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+//@Service
+public class FuelScheduler {
+
+
+}
